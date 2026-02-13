275f5b8a486d5b03f6207e75d6a9c155bd09b430
David Thompson <davthomp@redhat.com>
2026-02-13 11:57:41 -0500

    Fix extension to work on web
    Signed-off-by: David Thompson <davthomp@redhat.com>

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ const path = require('path');
 
 /** @type WebpackConfig */
 const extensionConfig = {
-  target: 'node', // VS Code extensions run in a Node.js-context 📖 -> https://webpack.js.org/configuration/node/
+  target: 'webworker',
 	mode: 'none', // this leaves the source code as close as possible to the original (when packaging we set this to 'production')
 
   entry: './src/extension.ts', // the entry point of this extension, 📖 -> https://webpack.js.org/configuration/entry-context/
@@ -24,7 +24,7 @@ const extensionConfig = {
     // modules added here also need to be added in the .vscodeignore file
   },
   resolve: {
-    // support reading TypeScript and JavaScript files, 📖 -> https://github.com/TypeStrong/ts-loader
+    mainFields: ['browser', 'module', 'main'],
     extensions: ['.ts', '.js']
   },
   module: {
